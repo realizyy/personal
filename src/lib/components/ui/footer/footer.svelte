b20ba7c6c4be02d6fed217371986b5c4256e79f3
Andri Faqih <realizy@student.telkomuniversity.ac.id>
2025-01-20 17:32:57 +0700

    update

--- src/lib/components/ui/footer/footer.svelte
+++ src/lib/components/ui/footer/footer.svelte
@@ -2,7 +2,9 @@
 	<div class="w-full py-3 px-4 md:px-8 lg:px-16 xl:px-24 2xl:px-32">
 		<div class="flex justify-between items-center">
 			<div class="flex items-center space-x-1.5">
-				<p class="self-center w-min px-2 pt-1 pb-[2px] mono lowercase text-yellow-300 border-yellow-300 border border-solid rounded-full">v1.2.0</p>
+				<p class="self-center w-min px-2 pt-1 pb-[2px] mono lowercase text-yellow-300 border-yellow-300 border border-solid rounded-full">
+					v1.2.1
+				</p>
 				<p>Last updated: January 20, { new Date().getFullYear() }</p>
 			</div>
 			<div class="flex flex-col items-start space-y-1">
